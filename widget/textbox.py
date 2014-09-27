ad695775b6a0be366209db3cb11ca960380fb2d3
Charles Vaillancourt <charles.vaillancourt@corp.ovh.ca>
2014-09-27 15:28:05 -0400

    started working on textbox widget

---
+++ widget/textbox.py
@@ -0,0 +1,21 @@
+from curses.textpad import Textbox
+
+from widget import Widget
+
+
+class _Textbox(Widget,Textbox):
+    """
+    curses.textpad.Textbox requires users to ^g on completion, which is sort
+    of annoying for an interactive chat client such as this, which typically only
+    reuquires an enter. This subclass fixes this problem by signalling completion
+    on Enter as well as ^g. Also, map <Backspace> key to ^h.
+    """
+    def __init__(*args, **kwargs):
+        Textbox.__init__(*args, **kwargs)
+
+    def do_command(self, ch):
+        if ch == 10: # Enter
+            return 0
+        if ch == 127: # Enter
+            return 8
+        return Textbox.do_command(self, ch)
